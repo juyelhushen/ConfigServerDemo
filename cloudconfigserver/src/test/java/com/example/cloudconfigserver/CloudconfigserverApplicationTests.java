3b880bbd9eddc16c78b3f896d68164f9ef574a6a
Juyel <juyelhushen@gmail.com>
2022-08-14 03:51:11 +0530

    initial commit

---
+++ cloudconfigserver/src/test/java/com/example/cloudconfigserver/CloudconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudconfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
