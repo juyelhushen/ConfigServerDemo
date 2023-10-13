6d4a0d8fc3b49e374427fb435cd7d09b0f4c8f55
Juyel <juyelhushen@gmail.com>
2023-10-13 15:34:48 +0530

    config client application included

---
+++ cloudconfigclient/src/main/java/com/jskool/cloudconfigclient/CloudconfigclientApplication.java
@@ -0,0 +1,13 @@
+package com.jskool.cloudconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudconfigclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudconfigclientApplication.class, args);
+	}
+
+}
